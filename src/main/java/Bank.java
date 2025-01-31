ae4a42a4d73c3843b91bc9b696a5f2c00d8c6a82
Morgan Browne <morgan.browne@keyin.com>
2025-01-31 12:54:58 -0330

    Started Bank Class

---
+++ src/main/java/Bank.java
@@ -0,0 +1,23 @@
+/**
+ * Bank Class:
+ * Used to create an Account.
+ */
+
+import java.util.Map;
+import java.util.HashMap;
+public class Bank {
+    private Map<String, BankAccount> accounts = new HashMap<>();
+
+    public void createAccount(String accountNumber, double initialBalance) {
+        if (!accounts.containsKey(accountNumber)) {
+            accounts.put(accountNumber, new BankAccount(accountNumber, initialBalance));
+        } else {
+            throw new IllegalArgumentException("Account Already Made.");
+        }
+
+        public BankAccount getAccount(String accountNumber) {
+            return accounts.get(accountNumber);
+        }
+    }
+
+}
